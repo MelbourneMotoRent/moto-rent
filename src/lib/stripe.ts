acbaf419055cdd100d9ee41c189e2777f3b02199
Glidego Car Rentals <MelbourneMotoRent@outlook.com>
2026-03-09 17:15:06 +0530

    Delete src directory

--- src/lib/stripe.ts
+++
@@ -1,9 +0,0 @@
-import Stripe from 'stripe';
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
-});
-
-export const BOND_AMOUNT = 50000;
-
-export const toCents = (amount: number) => Math.round(amount * 100);
